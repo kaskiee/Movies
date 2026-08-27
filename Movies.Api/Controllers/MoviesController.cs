9a0f889494826faab816da7fb854be3fb851d810
Katarzyna Krasnowska <krasnowskakat@gmail.com>
2026-08-27 17:44:10 +0200

    get endpoints added

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -14,6 +14,27 @@ public class MoviesController(IMovieRepository movieRepository) : ControllerBase
     {
         var movie = request.MapToMovie();
         await movieRepository.CreateAsync(movie);
-        return Created($"{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
+        return Created($"{ApiEndpoints.Movies.Create}/{movie.Id}", movie.MapToResponse());
+    }
+
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        var movie = await movieRepository.GetByIdAsync(id);
+        if (movie is null)
+        {
+            return NotFound();
+        }
+        var response = movie.MapToResponse();
+
+        return Ok(response);
+    }
+
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAll()
+    {
+        var movies = await movieRepository.GetAllAsync();
+        var response = movies.MapToResponse();
+        return Ok(response);
     }
 }
