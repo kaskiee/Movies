6b4e3d7148b06222a21f8c6e6809a99ac2a944f1
Katarzyna Krasnowska <krasnowskakat@gmail.com>
2026-08-27 17:30:09 +0200

    mapping added, api route extracted

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
 using Movies.Application.Models;
 using Movies.Application.Repositories;
 using Movies.Contracts.Requests;
@@ -6,20 +7,13 @@ using Movies.Contracts.Requests;
 namespace Movies.Api.Controllers;
 
 [ApiController]
-[Route("api")]
 public class MoviesController(IMovieRepository movieRepository) : ControllerBase
 {
-    [HttpPost("movies")]
+    [HttpPost(ApiEndpoints.Movies.Create)]
     public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
     {
-        var movie = new Movie
-        {
-            Id = Guid.NewGuid(),
-            Title = request.Title,
-            YearOfRelease = request.YearOfRelease,
-            Genres = request.Genres.ToList()
-        };
+        var movie = request.MapToMovie();
         await movieRepository.CreateAsync(movie);
-        return Created($"api/movies/{movie.Id}", movie);
+        return Created($"{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
     }
 }
