14e4abf479e27ef219aee760817de7699b339f83
Katarzyna Krasnowska <krasnowskakat@gmail.com>
2026-09-05 21:24:09 +0200

    jwt token generating controller added

---
+++ Movies.Contracts/Requests/Helpers/TokenGenerationRequest.cs
@@ -0,0 +1,10 @@
+namespace Movies.Contracts.Requests.Helpers;
+
+public class TokenGenerationRequest
+{
+    public Guid UserId { get; set; }
+    
+    public string Email { get; set; }
+
+    public Dictionary<string, object> CustomClaims { get; set; } = new();
+}
