6e44743b43f89f24ad241f48a03116ea09d86e62
Katarzyna Krasnowska <krasnowskakat@gmail.com>
2026-09-07 00:06:50 +0200

    fixed api routes and mappping

--- Movies.Api/ApiEndpoints.cs
+++ Movies.Api/ApiEndpoints.cs
@@ -13,8 +13,8 @@ public static class ApiEndpoints
         public const string Update = $"{Base}/{{id:guid}}";
         public const string Delete = $"{Base}/{{id:guid}}";
 
-        public const string Rate = $"{Base}/{{id:guid}}/ratings";
-        public const string DeleteRating = $"{Base}/{{id:guid}}/ratings";
+        public const string Rate = $"{Base}/{{movieId:guid}}/ratings";
+        public const string DeleteRating = $"{Base}/{{movieId:guid}}/ratings";
     }
     
     public static class Ratings
