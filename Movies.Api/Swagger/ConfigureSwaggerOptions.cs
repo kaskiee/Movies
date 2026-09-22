66c7ac61beac523c2f580a5b8c5cc5f1d3f3776f
Katarzyna Krasnowska <krasnowskakat@gmail.com>
2026-09-22 16:30:06 +0200

    versionning integration in Swagger added

---
+++ Movies.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,24 @@
+using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Movies.Api.Swagger;
+
+public class ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider, IHostEnvironment env)
+    : IConfigureOptions<SwaggerGenOptions>
+{
+    public void Configure(SwaggerGenOptions options)
+    {
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(
+                description.GroupName,
+                new OpenApiInfo
+                {
+                    Title = env.ApplicationName,
+                    Version = description.ApiVersion.ToString()
+                });
+        }
+    }
+}
