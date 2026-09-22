5d498a665dcb7a11b9bead542cd89045ef3aaef2
Katarzyna Krasnowska <krasnowskakat@gmail.com>
2026-09-22 23:46:10 +0200

    auth support in swagger added

--- Movies.Api/Swagger/ConfigureSwaggerOptions.cs
+++ Movies.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -20,5 +20,30 @@ public class ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider, IH
                     Version = description.ApiVersion.ToString()
                 });
         }
+        
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please provide a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
     }
 }
