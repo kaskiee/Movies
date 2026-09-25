9b4ff76be31ef5a4bc2d2a145a7f17ef7d4fccd0
Katarzyna Krasnowska <krasnowskakat@gmail.com>
2026-09-25 21:36:41 +0200

    endpoints definitions added to sdk

---
+++ Movies.Api.Sdk/IMoviesApi.cs
@@ -0,0 +1,33 @@
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+using Refit;
+
+namespace Movies.Api.Sdk;
+
+[Headers("Authorization: Bearer")]
+public interface IMoviesApi
+{
+    [Get(ApiEndpoints.Movies.Get)]
+    Task<MovieResponse> GetMovieAsync(string idOrSlug);
+
+    [Get(ApiEndpoints.Movies.GetAll)]
+    Task<MoviesResponse> GetAllMoviesAsync(GetAllMoviesRequest request);
+
+    [Post(ApiEndpoints.Movies.Create)]
+    Task<MovieResponse> CreateMovieAsync([Body] CreateMovieRequest request);
+
+    [Put(ApiEndpoints.Movies.Update)]
+    Task<MovieResponse> UpdateMovieAsync(Guid id, [Body] UpdateMovieRequest request);
+
+    [Delete(ApiEndpoints.Movies.Delete)]
+    Task DeleteMovieAsync(Guid id);
+
+    [Put(ApiEndpoints.Movies.Rate)]
+    Task RateMovieAsync(Guid movieId, [Body] RateMovieRequest request);
+
+    [Delete(ApiEndpoints.Movies.DeleteRating)]
+    Task DeleteRatingAsync(Guid movieId);
+
+    [Get(ApiEndpoints.Ratings.GetUserRatings)]
+    Task<IEnumerable<MovieRatingResponse>> GetUserRatingsAsync();
+}
